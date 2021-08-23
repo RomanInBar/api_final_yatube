37155378bace7d468dcfa700dc62db71805bdc26
Roman <barabinr@list.ru>
2021-08-24 01:05:15 +0300

    debuging after revie

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -44,7 +44,7 @@ class FollowViewSet(viewsets.ModelViewSet):
     search_fields = ('=user__username', '=following__username')
 
     def get_queryset(self):
-        return self.request.user.follower
+        return self.request.user.follower.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
